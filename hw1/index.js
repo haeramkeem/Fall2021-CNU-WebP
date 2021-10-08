a4d8f9803bb2979bcdfece3cdf43c3677c66c3e8
saltwalks <ewqdsacxz2345@gmail.com>
2021-10-09 01:25:40 +0900

    feat: modal open & cancel

--- hw1/index.js
+++ hw1/index.js
@@ -1,1 +1,16 @@
-document.write("hello world")
+/* Modal */
+// Show modal
+document.getElementById("add-item-btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    document.getElementById("modal-container").style.display = "block";
+});
+
+// Close Modal
+document.getElementById("cancel-item-btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    closeModal();
+});
+
+function closeModal() {
+    document.getElementById("modal-container").style.display = "none";
+}
