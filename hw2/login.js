58bea6346c8785b25c7c55a1bd41b64948029b02
haeramkeem <ewqdsacxz2345@gmail.com>
2021-10-31 16:33:36 +0900

    fix: login validate bug

--- hw2/login.js
+++ hw2/login.js
@@ -9,13 +9,23 @@ let modalVisible = false;
 function addLogInBtnClickEventListener() /* => void */ {
     $("#login-btn").click(() => {
         if(modalVisible) {
-            $("#modal-container").hide();
-            modalVisible = false;
+            hideModal();
         } else {
-            $("#modal-container").show();
-            modalVisible = true;
+            showModal();
         }
     });
+}
+
+function showModal() /* => void */ {
+    $("#modal-container").show();
+    modalVisible = true;
+}
+
+function hideModal() /* => void */ {
+    $("#modal-container").hide();
+    $("#id-input").val("");
+    $("#pw-input").val("");
+    modalVisible = false;
 }
 
 function addSubmitBtnClickEventListener() /* => void */ {
@@ -43,6 +53,9 @@ function idPwValidator() /* => {id: string, pw: string} */ {
     const pw = $("#pw-input").val();
     if(!id || !pw || !/^([A-Za-z0-9]){6,15}$/.test(id) || !/^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/.test(pw)) {
         alert("아이디 또는 패스워드의 입력양식을 체크해주세요");
+        if(id || pw) {
+            hideModal();
+        }
         return {id: "", pw: ""};
     }
     return {id, pw};
