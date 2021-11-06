81b662f747947230bb468b8902ac6810a4f80a22
haeramkeem <ewqdsacxz2345@gmail.com>
2021-11-06 11:41:40 +0900

    fix: drag / drop after read bug

--- prac8/index.js
+++ prac8/index.js
@@ -1,28 +1,27 @@
 const eventDataSend = (event) => { event.originalEvent.dataTransfer.setData('text',event.target.id)};
 const setPreventDefault = (event) => { event.preventDefault(); }
 
-function incIdx() {
-    const idx = parseInt(localStorage.getItem("idx"))
-    localStorage.setItem("idx", idx + 1);
-}
-
 function render() {
     $("#plans").html("");
     $("#today").html("");
     $("#tomorrow").html("");
     for(let i = 0; i < localStorage.length; i++) {
         const eid = localStorage.key(i);
-        if(eid === "idx") {continue;}
+        if(eid === "idx") { continue; }
         const par = localStorage.getItem(eid).split("->")[0];
         const plan = localStorage.getItem(eid).split("->")[1];
         switch(par) {
             case "plans" :
-                $("#plans").append(`<div class="plan" id="${eid}" draggable="true">${plan}</div>`); break;
+                $("#plans").append(`<div class="plan" id="${eid}" draggable="true">${plan}</div>`);
+                break;
             case "today" : 
-                $("#today").append(`<div class="plan" id="${eid}" draggable="true">${plan}</div>`); break;
+                $("#today").append(`<div class="plan" id="${eid}" draggable="true">${plan}</div>`);
+                break;
             case "tomorrow" : 
-                $("#tomorrow").append(`<div class="plan" id="${eid}" draggable="true">${plan}</div>`); break;
+                $("#tomorrow").append(`<div class="plan" id="${eid}" draggable="true">${plan}</div>`);
+                break;
         }
+        $(`#${eid}`).on("dragstart", eventDataSend);
     }
 }
 
@@ -34,16 +33,10 @@ $(document).ready(function(){
 
     // add plan
     $("#add-plan-btn").click(() => {
-        const plan = $("#add-plan-input").val();
+        localStorage.setItem(`plan-${localStorage.getItem("idx")}`, `plans->${$("#add-plan-input").val()}`);
         $("#add-plan-input").val("");
-
-        const idx = localStorage.getItem("idx");
-        const id = `plan-${idx}`;
-
-        localStorage.setItem(id, `plans->${plan}`);
-        $("#plans").append(`<div class="plan" id="${id}" draggable="true">${plan}</div>`);
-        $(`#${id}`).on("dragstart", eventDataSend);
-        incIdx();
+        render();
+        localStorage.setItem("idx", parseInt(localStorage.getItem("idx")) + 1);
     });
 
     // read btn
@@ -54,7 +47,7 @@ $(document).ready(function(){
     // clear btn
     $("#clearBtn").click(function() {
         $(".plan").each((idx, el) => {
-            if(localStorage.getItem(el.id) === "plans") { return; }
+            if(localStorage.getItem(el.id).startsWith("plans")) { return; }
             localStorage.removeItem(el.id);
         });
         render();
